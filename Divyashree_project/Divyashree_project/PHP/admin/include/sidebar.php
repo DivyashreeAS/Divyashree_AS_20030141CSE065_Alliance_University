296f71ce9f24f4a062ef93c9dac1027261709fc2
DivyashreeAS <133731896+DivyashreeAS@users.noreply.github.com>
2023-12-02 16:26:00 +0530

    added

---
+++ Divyashree_project/Divyashree_project/PHP/admin/include/sidebar.php
@@ -0,0 +1,72 @@
+
+		<div class="loader-bg">
+		<div class="loader-track">
+			<div class="loader-fill"></div>
+		</div>
+	</div>
+	<nav class="pcoded-navbar  ">
+		<div class="navbar-wrapper  ">
+			<div class="navbar-content scroll-div " >
+				
+				<div class="">
+					<div class="main-menu-header">
+						<img class="img-radius" src="assets/images/user/user-gear.png" alt="User-Profile-Image">
+						<div class="user-details">
+							<?php
+$id=intval($_SESSION["aid"]);
+$query=mysqli_query($con,"select * from admin where id='$id'");
+while($row=mysqli_fetch_array($query))
+{
+?>	
+							<span><?php echo  htmlentities($row['fullname']);?></span>
+							<div id="more-details"><?php echo  htmlentities($row['email']);?><i class="fa fa-chevron-down m-l-5"></i></div><?php } ?>
+						</div>
+					</div>
+					<div class="collapse" id="nav-user-link">
+						<ul class="list-unstyled">
+							<li class="list-group-item"><a href="admin-profile.php"><i class="feather icon-user m-r-5"></i>View Profile</a></li>
+							<li class="list-group-item"><a href="setting.php"><i class="feather icon-settings m-r-5"></i>Settings</a></li>
+							<li class="list-group-item"><a href="logout.php"><i class="feather icon-log-out m-r-5"></i>Logout</a></li>
+						</ul>
+					</div>
+				</div>
+				
+				<ul class="nav pcoded-inner-navbar ">
+					<li class="nav-item pcoded-menu-caption">
+						<label>Admin Management</label>
+					</li>
+					<li class="nav-item">
+					    <a href="dashboard.php" class="nav-link "><span class="pcoded-micon"><i class="feather icon-home"></i></span><span class="pcoded-mtext">Dashboard</span></a>
+					</li>
+					
+					<li class="nav-item">
+					    <a href="category.php" class="nav-link "><span class="pcoded-micon"><i class="feather icon-file-text"></i></span><span class="pcoded-mtext">Add Category</span></a>
+					</li>
+					<li class="nav-item">
+					    <a href="subcategory.php" class="nav-link "><span class="pcoded-micon"><i class="feather icon-file-text"></i></span><span class="pcoded-mtext">Add Subcategory</span></a>
+					</li>
+					<li class="nav-item">
+					    <a href="state.php" class="nav-link "><span class="pcoded-micon"><i class="feather icon-file-text"></i></span><span class="pcoded-mtext">Add State</span></a>
+					</li>
+					<li class="nav-item">
+					    <a href="manage-users.php" class="nav-link "><span class="pcoded-micon"><i class="feather icon-user"></i></span><span class="pcoded-mtext">Manage Users</span></a>
+					</li>
+
+
+
+
+
+
+
+					
+					
+					
+					
+
+				</ul>
+				
+		
+				
+			</div>
+		</div>
+	</nav>
---
+++ Divyashree_project/Divyashree_project/PHP/index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <title>Complaint Management</title>
+   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+   <link rel="stylesheet" type="text/css" href="css/style.css">
+   <link rel="stylesheet" href="css/responsive.css">
+   <link rel="icon" href="images/fevicon.png" type="image/gif" />
+   <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+   <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+   <!-- fonts -->
+   <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Poppins:400,700&display=swap" rel="stylesheet">
+   <!-- owl stylesheets -->
+   <link rel="stylesheet" href="css/owl.carousel.min.css">
+   <link rel="stylesheet" href="css/owl.theme.default.min.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
+      media="screen">
+      
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.3/font/bootstrap-icons.min.css">
+
+</head>
+
+<body>
+   <div class="header_section">
+      <div class="container-fluid ">
+         <div class="row">
+            <div class="col-sm-2 col-6">
+               <a class="logo" href="index.php"><h1 style="font-size: 40px;color: white;"><img src="images/cms.png" alt=""></h1></a>
+            </div>
+            <div class="col-sm-8 col-6">
+               <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                     <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNav" style="color:black;">
+                     <ul class="navbar-nav" style="
+    margin: -5px;
+    height: 95px;
+    list-style: auto;
+    display: flex;
+    align-items: center;
+    ">
+                        <li class="nav-item active">
+                           <a class="nav-link" href="index.php" style="color:black;">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                           <a class="nav-link" href="admin/index.php" style="color:black;">admin</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="user/index.php" style="color:black;">User Login</a>
+                        </li>
+                        <li class="nav-item">
+                           <a class="nav-link" href="user/registration.php" style="color:black;">User Regsitration</a>
+                        </li>
+                       
+                        
+                     </ul>
+                  </div>
+               </nav>
+            </div>
+         
+         </div>
+      </div>
+      <div class="banner_section layout_padding">
+         <div class="container">
+            <div id="main_slider" class="carousel slide" data-ride="carousel">
+               <div class="carousel-inner">
+                  <div class="carousel-item active">
+                     <h1 class="banner_taital">Lodge Your Complaint </h1>
+                     <div class="read_bt"></div>
+                     <div class="number_main">
+                        <div class="number_text">01</div>
+                     </div>
+                  </div>
+                  <div class="carousel-item">
+                     <h1 class="banner_taital">Feel Free To Complaint</h1>
+                     <div class="read_bt"></div>
+                     <div class="number_main">
+                        <div class="number_text">02</div>
+                     </div>
+                  </div>
+     
+               </div>
+               <a class="carousel-control-prev" href="#main_slider" role="button" data-slide="prev">
+                  <i class="fa fa-angle-left"></i>
+               </a>
+               <a class="carousel-control-next" href="#main_slider" role="button" data-slide="next">
+                  <i class="fa fa-angle-right"></i>
+               </a>
+            </div>
+         </div>
+      </div>
+   </div>
+   <script src="js/jquery.min.js"></script>
+   <script src="js/popper.min.js"></script>
+   <script src="js/bootstrap.bundle.min.js"></script>
+   <script src="js/jquery-3.0.0.min.js"></script>
+   <script src="js/plugin.js"></script>
+   <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+   <script src="js/custom.js"></script>
+   <script src="js/owl.carousel.js"></script>
+   <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+   <div class="container">
+  <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+    <div class="col mb-3">
+      <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+      </a>
+      <p class="text-body-secondary">© 2023</p>
+    </div>
+
+    <div class="col mb-3">
+
+    </div>
+    <div class="col mb-3">
+      <h5>Section</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="index.php" class="nav-link p-0 text-body-secondary">HOME</a></li>
+        <li class="nav-item mb-2"><a href="admin/index.php" class="nav-link p-0 text-body-secondary">ADMIN</a></li>
+        <li class="nav-item mb-2"><a href="user/index.php" class="nav-link p-0 text-body-secondary">USER LOGIN</a></li>
+        <li class="nav-item mb-2"><a href="user/registration.php" class="nav-link p-0 text-body-secondary">USER REGISTRATION</a></li>
+      </ul>
+    </div>
+
+    <div class="col mb-3">
+      <h5>Contact US</h5>
+      <ul class="nav flex-column">
+         <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><i class="bi bi-phone me-3"></i>+919113840336</li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><i class="bi bi-phone me-3"></i>+916362205626</a></li>
+      <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary"><i class="bi bi-envelope me-3"></i>complaint@gmail.com</a></li>
+      </ul>
+      
+    </div>
+    
+    <div class="col mb-3">
+      <h5>About US</h5>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae eveniet perspiciatis illum modi rerum atque </p>
+      
+    </div>
+    
+  </footer>
+  <div class="copyright_section">
+      <div class="container">
+         <p class="copyright_text">Complaint Management System.</p>
+      </div>
+   </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
